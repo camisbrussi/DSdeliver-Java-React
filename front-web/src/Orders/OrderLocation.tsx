fad159844bb0e6698814a1e262cea7741e0a3144
camisbrussi <camila.sbrussi@universo.univates.br>
2021-01-09 09:16:45 -0300

    Finish front web

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import AsyncSelect from "react-select/async";
 import { fetchLocalMapBox } from "../api";
-import { OrderLocationdata } from "./types";
+import { OrderLocationData } from "./types";
 
 const initialposition = {
   lat: -29.4568855,
@@ -20,7 +20,7 @@ type Place = {
 }
 
 type Props = {
-    onChangeLocation: (location: OrderLocationdata) => void;
+    onChangeLocation: (location: OrderLocationData) => void;
 }
 
 function OrderLocation({ onChangeLocation }: Props) {
@@ -76,7 +76,7 @@ function OrderLocation({ onChangeLocation }: Props) {
         center={address.position} 
         zoom={15}
         key={address.position.lat} 
-        scrollWheelZoom>
+        scrollWheelZoom={false}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,30 +1,82 @@
-import React, { useEffect, useState } from 'react';
-import { fetchProduct } from '../api';
-import OrderLocation from './OrderLocation';
-import ProductsList from './ProductsList';
-import StepsHeader from './StepsHeader';
-import './styles.css';
-import { OrderLocationdata, Product } from './types';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import React, { useEffect, useState } from "react";
+import { fetchProduct, saveOrder } from "../api";
+import Footer from "../Footer";
+import { checkIsSelected } from "./helpers";
+import OrderLocation from "./OrderLocation";
+import OrderSumary from "./OrderSumary";
+import ProductsList from "./ProductsList";
+import StepsHeader from "./StepsHeader";
+import { OrderLocationData, Product } from "./types";
+import "./styles.css";
 
 
-function  Orders(){
-    const [products, setProducts] = useState<Product[]>([]);
-    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
+  const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+  const totalPrice = selectedProducts.reduce((sum, item) => {
 
+    return sum + item.price;
+  }, 0);
 
-    useEffect(() =>{
-        fetchProduct()
-        .then(response => setProducts(response.data))
-        .catch(error => console.log(error))
-    }, []);
+  useEffect(() => {
+    fetchProduct()
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
-    return(
-       <div className="orders-container">
-           <StepsHeader />
-           <ProductsList products={products} />
-           <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
-       </div>
-    )
+  const handleSelectProduct = (product: Product) => {
+    const isAlreadySelected = checkIsSelected(selectedProducts, product);
+
+    if (isAlreadySelected) {
+      const selected = selectedProducts.filter(
+        (item) => item.id !== product.id)
+      setSelectedProducts(selected);
+    } else {
+      setSelectedProducts((previous) => [...previous, product]);
+    }
+  };
+
+  const handleSubmit = () => {
+    const productsIds = selectedProducts.map(({ id }) => ({ id }));
+    const payload = {
+      ...orderLocation!,
+      products: productsIds
+    }
+  
+    saveOrder(payload).then((response) => {
+      toast.error(`Pedido enviado com sucesso! N° ${response.data.id}`);
+      setSelectedProducts([]);
+    })
+      .catch(() => {
+        toast.warning('Erro ao enviar pedido');
+      })
+  }
+  
+
+  return (
+    <>
+      <div className="orders-container">
+        <StepsHeader />
+        <ProductsList
+          products={products}
+          onSelectProduct={handleSelectProduct}
+          selectedProducts={selectedProducts}
+        />
+        <OrderLocation
+          onChangeLocation={(location) => setOrderLocation(location)}
+        />
+        <OrderSumary 
+        amount={selectedProducts.length} 
+        totalPrice={totalPrice} 
+        onSubmit={handleSubmit}
+        />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Orders;
