c321da3f1e8d889bc21bed8895d3b9f710acfd17
camisbrussi <camila.sbrussi@universo.univates.br>
2021-01-11 14:40:36 -0300

    Add orders list

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, ScrollView, Image } from "react-native";
+import Header from "../Header";
+import OrderCard from "../OrderCard";
+
+export default function Orders() {
+  return (
+    <>
+    <Header />
+    <ScrollView style={styles.container}>
+      <OrderCard />
+      <OrderCard />
+      <OrderCard />
+      <OrderCard />
+      <OrderCard />
+    </ScrollView>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingRight: '5%',
+    paddingLeft: '5%'
+  }
+});
